49db1e66854d6671d3685eb8aa83195421a3bcec
quangnd <quangnd.nta@gmail.com>
2022-12-26 08:51:47 +0700

    Init

---
+++ swift-learn/swift_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_learnApp.swift
+//  swift-learn
+//
+//  Created by quangnguyen on 26/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct swift_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
